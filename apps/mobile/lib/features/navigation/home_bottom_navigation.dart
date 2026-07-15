cf9f8455277aaa8ec55b6bc632fa060e0948c0cf
Westyoon <yoonseo1130@naver.com>
2026-07-15 18:52:26 +0900

    style(mobile): apply custom home navigation icons

--- apps/mobile/lib/features/navigation/home_bottom_navigation.dart
+++ apps/mobile/lib/features/navigation/home_bottom_navigation.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-import '../../core/widgets/pixel_glyph.dart';
-
 enum HomeMenuAction { files, games, feeding, wardrobe }
+
+const _menuIconAssets = <HomeMenuAction, String>{
+  HomeMenuAction.files: 'assets/images/home_navigation/folder.png',
+  HomeMenuAction.games: 'assets/images/home_navigation/console.png',
+  HomeMenuAction.feeding: 'assets/images/home_navigation/cheese.png',
+  HomeMenuAction.wardrobe: 'assets/images/home_navigation/hanger.png',
+};
 
 class HomeBottomNavigation extends StatelessWidget {
   const HomeBottomNavigation({
@@ -30,52 +35,49 @@ class HomeBottomNavigation extends StatelessWidget {
         height: 66,
         child: Row(
           children: [
-            _item(HomeMenuAction.files, PixelGlyph.folder, '파일 목록'),
-            _item(HomeMenuAction.games, PixelGlyph.puzzle, '미니게임'),
-            _item(HomeMenuAction.feeding, PixelGlyph.cheese, '치즈 주기'),
-            _item(HomeMenuAction.wardrobe, PixelGlyph.hanger, '생쥐 의상실'),
+            _item(HomeMenuAction.files, '파일 목록'),
+            _item(HomeMenuAction.games, '미니게임'),
+            _item(HomeMenuAction.feeding, '치즈 주기'),
+            _item(HomeMenuAction.wardrobe, '생쥐 의상실'),
           ],
         ),
       ),
     ),
   );
 
-  Widget _item(HomeMenuAction action, PixelGlyph glyph, String tooltip) =>
-      Expanded(
-        child: Semantics(
-          button: true,
-          selected: selected == action,
-          label: tooltip,
-          child: Tooltip(
-            message: tooltip,
-            child: InkWell(
-              key: ValueKey('home-menu-${action.name}'),
-              onTap: () => onSelected(action),
-              child: AnimatedContainer(
-                duration: const Duration(milliseconds: 120),
-                margin: const EdgeInsets.all(6),
-                decoration: BoxDecoration(
-                  color: selected == action
-                      ? const Color(0xFFFFD778)
-                      : Colors.transparent,
-                  border: selected == action
-                      ? Border.all(color: const Color(0xFF3A2A1F), width: 2)
-                      : null,
-                ),
-                alignment: Alignment.center,
-                child: PixelGlyphIcon(
-                  glyph,
-                  size: 38,
-                  accentColor: switch (action) {
-                    HomeMenuAction.files => const Color(0xFF9BC7F5),
-                    HomeMenuAction.games => const Color(0xFFE78596),
-                    HomeMenuAction.feeding => const Color(0xFFFFC857),
-                    HomeMenuAction.wardrobe => const Color(0xFFB9A7E8),
-                  },
-                ),
-              ),
+  Widget _item(HomeMenuAction action, String tooltip) => Expanded(
+    child: Semantics(
+      button: true,
+      selected: selected == action,
+      label: tooltip,
+      child: Tooltip(
+        message: tooltip,
+        child: InkWell(
+          key: ValueKey('home-menu-${action.name}'),
+          onTap: () => onSelected(action),
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 120),
+            margin: const EdgeInsets.all(6),
+            decoration: BoxDecoration(
+              color: selected == action
+                  ? const Color(0xFFFFD778)
+                  : Colors.transparent,
+              border: selected == action
+                  ? Border.all(color: const Color(0xFF3A2A1F), width: 2)
+                  : null,
+            ),
+            alignment: Alignment.center,
+            child: Image.asset(
+              _menuIconAssets[action]!,
+              key: ValueKey('home-menu-icon-${action.name}'),
+              width: 46,
+              height: 46,
+              fit: BoxFit.contain,
+              filterQuality: FilterQuality.none,
             ),
           ),
         ),
-      );
+      ),
+    ),
+  );
 }
--- apps/mobile/test/home_bottom_navigation_test.dart
+++ apps/mobile/test/home_bottom_navigation_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mousekeeper/features/navigation/home_bottom_navigation.dart';
 
 void main() {
-  testWidgets('four pixel menu actions are interactive and evenly available', (
+  testWidgets('four provided menu icons are interactive and evenly available', (
     tester,
   ) async {
     HomeMenuAction? selected;
@@ -20,11 +20,21 @@ void main() {
       ),
     );
 
+    const expectedAssets = <HomeMenuAction, String>{
+      HomeMenuAction.files: 'assets/images/home_navigation/folder.png',
+      HomeMenuAction.games: 'assets/images/home_navigation/console.png',
+      HomeMenuAction.feeding: 'assets/images/home_navigation/cheese.png',
+      HomeMenuAction.wardrobe: 'assets/images/home_navigation/hanger.png',
+    };
     for (final action in HomeMenuAction.values) {
       expect(find.byKey(ValueKey('home-menu-${action.name}')), findsOneWidget);
+      final image = tester.widget<Image>(
+        find.byKey(ValueKey('home-menu-icon-${action.name}')),
+      );
+      expect(image.image, isA<AssetImage>());
+      expect((image.image as AssetImage).assetName, expectedAssets[action]);
+      await tester.tap(find.byKey(ValueKey('home-menu-${action.name}')));
+      expect(selected, action);
     }
-
-    await tester.tap(find.byKey(const ValueKey('home-menu-feeding')));
-    expect(selected, HomeMenuAction.feeding);
   });
 }
